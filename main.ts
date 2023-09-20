5685fb1974e590387374385379db8aa9cf796a37
Marcin <mwoz123@git>
2023-09-20 14:30:43 +0200

    feat: ignore whitespace

--- main.ts
+++ main.ts
@@ -2,18 +2,34 @@ import { Plugin, MarkdownView, Editor } from "obsidian";
 
 export default class SwissArmyKnifePlugin extends Plugin {
 	async onload() {
-
 		this.addCommand({
 			id: "replace-doubled-empty-lines-with-single",
 			name: "Replace doubled empty lines with single",
-			editorCallback: (editor: Editor, view: MarkdownView) => this.replaceDoubledEmptyLinesWithSinge(editor),
+			editorCallback: (editor: Editor, view: MarkdownView) => this.replaceDoubledEmptyLinesWithSingle(editor),
+		});
+		this.addCommand({
+			id: "remove-empty-lines",
+			name: "Remove empty lines",
+			editorCallback: (editor: Editor, view: MarkdownView) => this.removeEmptyLines(editor),
 		});
 	}
 
-	replaceDoubledEmptyLinesWithSinge(editor: Editor) {
-		const currentText = editor.getValue();
-		const doubledEmptyLinesRegex = /^\n\n/gm;
-		const updatedText = currentText.replace(doubledEmptyLinesRegex, '\n')
-		editor.setValue(updatedText);
+	replaceDoubledEmptyLinesWithSingle(editor: Editor) {
+		const doubledEmptyLinesWithOptionalWhiteSpacesRegex = /^\s*?\n\s*?\n/gm;
+		return replaceRegexInFile(editor, doubledEmptyLinesWithOptionalWhiteSpacesRegex, '\n');
 	}
+
+	removeEmptyLines(editor: Editor) {
+		const doubledEmptyLinesWithOptionalWhiteSpacesRegex = /\s*?\n\s*?\n$/gm;
+		return replaceRegexInFile(editor, doubledEmptyLinesWithOptionalWhiteSpacesRegex, '\n');
+	}
+
 }
+
+function replaceRegexInFile(editor: Editor, pattern: RegExp | string, replacement: string) {
+	const currentText = editor.getValue();
+	const updatedText = currentText.replace(pattern, replacement)
+	editor.setValue(updatedText);
+}
+
+
